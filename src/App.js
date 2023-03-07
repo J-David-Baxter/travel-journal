775e85f99a109e866db2bcf8c08e0d6d35729401
J-David-Baxter <jdavidbx@gmail.com>
2023-03-07 05:34:56 -0500

    Create nav component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Nav from './components/Nav';
+import Card from './components/Card';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
-    <div className='App'>App</div>
+    <div className='App'>
+      <Nav />
+      <Card />
+    </div>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='Nav'>
+        <img src='../images/Fill.png' alt='world'/>
+        <h1>my travel journal.</h1>
+    </div>
+  )
+}
+
+export default Nav
